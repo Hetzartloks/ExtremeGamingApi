6652323d0890ff1f8d40e79b60ebf9b7d431d9d2
Dunkan Hernandez <dun.hernandez@duocuc.cl>
2025-05-29 05:15:17 -0400

    Seguridad
    Nuevo nivel de seguridad

--- routes/categories.js
+++ routes/categories.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 const ctrl   = require('../controllers/categoriesController');
+const auth = require('../middlewares/authMiddleware');
 
 router.get('/',         ctrl.list       );
 router.post('/search',  ctrl.getByName  );
-router.post('/',        ctrl.create     );
-router.put('/:id',      ctrl.update     );
-router.delete('/:id',   ctrl.remove     );
+router.post('/',        auth, ctrl.create     );
+router.put('/:id',      auth, ctrl.update     );
+router.delete('/:id',   auth, ctrl.remove     );
 
 module.exports = router;
--- routes/games.js
+++ routes/games.js
@@ -1,12 +1,13 @@
 // routes/games.js
 const router = require('express').Router();
 const ctrl   = require('../controllers/gamesController');
+const auth = require('../middlewares/authMiddleware');
 
-router.get('/',    ctrl.list);
-router.get('/:id', ctrl.getById);
-router.post('/search', ctrl.getByName);
-router.post('/',   ctrl.create);
-router.put('/:id', ctrl.update);
-router.delete('/:id', ctrl.remove);
+router.get('/',         ctrl.list);
+router.get('/:id',      ctrl.getById);
+router.post('/search',  ctrl.getByName);
+router.post('/',        auth, ctrl.create);
+router.put('/:id',      auth, ctrl.update);
+router.delete('/:id',   auth, ctrl.remove);
 
 module.exports = router;
